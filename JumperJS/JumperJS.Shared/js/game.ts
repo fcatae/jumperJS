ee1097eb91399502894370ec571545ada4edcdc8
fcatae <fabricio.catae@microsoft.com>
2014-08-17 18:18:00 -0300

    sound.js

--- JumperJS/JumperJS.Shared/js/game.ts
+++ JumperJS/JumperJS.Shared/js/game.ts
@@ -235,21 +235,3 @@ function Respawn() {
         }
     }
 }
-
-var audiomanifest = [
-    { id: "jump", src: "jump.wav" }, 
-    { id: "intro", src: "intro.mp3" },
-    { id: "game", src: "sparkman.mp3" },
-    { id: "gameover", src: "gameover.mp3" },
-    
-];
-
-// HACK
-// break on the phone
-// createjs.Sound.EXTENSION_MAP["wav"] = "mp3";
-
-createjs.Sound.initializeDefaultPlugins();
-
-createjs.Sound.registerManifest(audiomanifest, "sounds/");
-
-createjs.Sound.play("game", { loop: -1 } );
---
+++ JumperJS/JumperJS.Shared/js/sound.js
@@ -0,0 +1,18 @@
+﻿
+var audiomanifest = [
+    { id: "jump", src: "jump.wav" },
+    { id: "intro", src: "intro.mp3" },
+    { id: "game", src: "sparkman.mp3" },
+    { id: "gameover", src: "gameover.mp3" },
+
+];
+
+// HACK
+// break on the phone
+// createjs.Sound.EXTENSION_MAP["wav"] = "mp3";
+
+createjs.Sound.initializeDefaultPlugins();
+
+createjs.Sound.registerManifest(audiomanifest, "sounds/");
+
+createjs.Sound.play("game", { loop: -1 });
