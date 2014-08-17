903d2607f05a3753f3c3941676164c676057e4cf
fcatae <fabricio.catae@microsoft.com>
2014-08-17 18:36:37 -0300

    background.js

---
+++ JumperJS/JumperJS.Shared/js/background.js
@@ -0,0 +1,38 @@
+﻿
+// Background
+var background = new createjs.Bitmap("images/bg.png");
+background.y = -4255 + window.outerHeight + 3;
+stage.addChild(background);
+
+// Floor data
+var floordata = {
+    images: ["images/floor.png", "images/floor2.png"],
+    frames: [[0, 0, 114, 18, 0], [0, 0, 114, 38, 1]],
+    animations: {
+        normal: [0, 0, false, 1],
+        bounce: { frames: [1, 1, 0, 1], speed: .5, next: "normal" }
+    }
+};
+
+var fsheet = new createjs.SpriteSheet(floordata);
+stage.addChild(floor);
+
+// floor list
+var floorlist = new Array(7);
+var floorlist_distance = 200;
+var floorlist_respawn_distance = floorlist.length * floorlist_distance;
+
+for (var i = 0; i < floorlist.length; i++) {
+
+    var floor = new createjs.Sprite(fsheet, "normal");
+    floor.x = 500 * Math.random();
+    floor.y = 900 - i * floorlist_distance;
+    floor.force = { min: 30, mul: 1.1, max: 50 };
+    floor.force = { min: 50, mul: 1.1, max: 50 };
+
+    //floor.regY = -20;
+    stage.addChild(floor);
+    floorlist[i] = floor;
+}
+
+
--- JumperJS/JumperJS.Shared/js/game.ts
+++ JumperJS/JumperJS.Shared/js/game.ts
@@ -1,101 +1,57 @@
 ﻿///<reference path="../typings/createjs/createjs.d.ts" />
 ///<reference path="init.ts"/>
 
-// Background
-var background = new createjs.Bitmap("images/bg.png");
-background.y = -4255 + window.outerHeight + 3;
-stage.addChild(background);
-
-// Floor data
-var floordata = {
-    images: ["images/floor.png", "images/floor2.png"],
-    frames: [[0, 0, 114, 18, 0], [0, 0, 114, 38, 1]],
-    animations: { 
-        normal: [0, 0, false, 1],
-        bounce: { frames: [1,1,0,1], speed: .5, next: "normal" }
-    }
-};
-
-var fsheet = new createjs.SpriteSheet(floordata);
-//var floor = new createjs.Sprite(fsheet, "normal");
-//floor.x = 0;
-//floor.y = 700;
-//floor.force = { min: 5, mul: .9 };
-//floor.regY = -20;
-
-stage.addChild(floor);
-
-// floor list
-var floorlist = new Array(7);
-var floorlist_distance = 200;
-var floorlist_respawn_distance = floorlist.length * floorlist_distance;
-
-for (var i = 0; i < floorlist.length; i++) {
-
-    var floor = new createjs.Sprite(fsheet, "normal");
-    floor.x = 500 * Math.random();
-    floor.y = 900 - i * floorlist_distance;
-    floor.force = { min: 30, mul: 1.1, max: 50 };
-    floor.force = { min: 50, mul: 1.1, max: 50 };
-
-    //floor.regY = -20;
-    stage.addChild(floor);
-    floorlist[i] = floor;
-}
 
 
+//var texturedata = {
+//    "images": ["images/mega.png"],
+//    "frames": [
+//        [2, 2, 84, 143],
+//        [2, 147, 60, 144],
+//        [2, 292, 71, 144],
+//        [2, 437, 74, 144],
+//        [2, 582, 86, 144],
+//        [2, 728, 81, 144],
+//        [2, 874, 94, 144]
+//    ],
+//    animations: {
+//        jump: [0, 2, false, .5],
+//        fall: [3, 4, false, .1],
+//        stop: [5, 5]
+//    }
+//};
+
+//var sprite = new createjs.Container();
+
+//window.showTarget = true;
+//if (window.showTarget) {
+//    var target = new createjs.Bitmap("images/target.png");
+//    target.x = target.y = 0;
+//    target.scaleX = target.scaleY = .5;
+//    sprite.addChild(target);
+//}
+
+//var ss = new createjs.SpriteSheet(texturedata);
+//var spriteImage = new createjs.Sprite(ss, "jump");
+//spriteImage.scaleX = spriteImage.scaleY = .8;
+//spriteImage.regX = 70 * spriteImage.scaleX / 2;
+//spriteImage.regY = 144 * spriteImage.scaleY;
+//sprite.addChild(spriteImage);
+
+//sprite.x = 50;
+
+//stage.addChild(sprite);
 
 
+//sprite.vx = 0;
 
-var texturedata = {
-    "images": ["images/mega.png"],
-    "frames": [
-        [2, 2, 84, 143],
-        [2, 147, 60, 144],
-        [2, 292, 71, 144],
-        [2, 437, 74, 144],
-        [2, 582, 86, 144],
-        [2, 728, 81, 144],
-        [2, 874, 94, 144]
-    ],
-    animations: {
-        jump: [0, 2, false, .5],
-        fall: [3, 4, false, .1],
-        stop: [5, 5]
-    }
-};
+//// queda
+//sprite.vy = 1;
+//sprite.ay = 3;
+//sprite.max_vy = 50;
 
-var sprite = new createjs.Container();
-
-window.showTarget = true;
-if (window.showTarget) {
-    var target = new createjs.Bitmap("images/target.png");
-    target.x = target.y = 0;
-    target.scaleX = target.scaleY = .5;
-    sprite.addChild(target);
-}
-
-var ss = new createjs.SpriteSheet(texturedata);
-var spriteImage = new createjs.Sprite(ss, "jump");
-spriteImage.scaleX = spriteImage.scaleY = .8;
-spriteImage.regX = 70 * spriteImage.scaleX / 2;
-spriteImage.regY = 144 * spriteImage.scaleY;
-sprite.addChild(spriteImage);
-
-sprite.x = 50;
-
-stage.addChild(sprite);
-
-
-sprite.vx = 0;
-
-// queda
-sprite.vy = 1;
-sprite.ay = 3;
-sprite.max_vy = 50;
-
-var currentState = (sprite.vy < 0) ? "jump" : "fall";
-spriteImage.gotoAndPlay(currentState);
+//var currentState = (sprite.vy < 0) ? "jump" : "fall";
+//spriteImage.gotoAndPlay(currentState);
 
 
 
