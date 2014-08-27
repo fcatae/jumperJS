e3b25c491c8a1e43732cd5b67d1f568a1a548627
fcatae <fabricio.catae@microsoft.com>
2014-08-27 18:07:08 -0300

    commit teste

--- JumperJS/JumperJS.Shared/js/sound.js
+++ JumperJS/JumperJS.Shared/js/sound.js
@@ -1,23 +1,24 @@
 ﻿
-var audiomanifest = [
-    { id: "jump", src: "jump.wav" },
-    { id: "intro", src: "intro.mp3" },
-    { id: "game", src: "sparkman.mp3" },
-    { id: "gameover", src: "gameover.mp3" },
+function sound_init() {
 
-];
+    createjs.Sound.play("game", { loop: -1 });
 
-// HACK
-// break on the phone
-createjs.Sound.EXTENSION_MAP["wav"] = "mp3";
+//var audiomanifest = [
+//    { id: "jump", src: "jump.mp3" },
+//    { id: "intro", src: "intro.mp3" },
+//    { id: "game", src: "sparkman.mp3" },
+//    { id: "gameover", src: "gameover.mp3" },
 
-createjs.Sound.initializeDefaultPlugins();
+//];
 
-createjs.Sound.addEventListener("fileload", function (evt) {
-    var a = evt.src;
-    var b = 1;
-});
+//// HACK
+//// break on the phone
+////createjs.Sound.EXTENSION_MAP["wav"] = "mp3";
 
-createjs.Sound.registerManifest(audiomanifest, "sounds/");
+//createjs.Sound.initializeDefaultPlugins();
 
-//createjs.Sound.play("game", { loop: -1 });
+
+//createjs.Sound.registerManifest(audiomanifest, "sounds/");
+
+
+}
